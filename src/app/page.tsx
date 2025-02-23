c7a2a871229815a34db0715220cdc8cd16a35219
veresur <veres.tamas@pm.me>
2025-02-23 23:12:39 +0100

    FNTK-13 charts refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
-
 export default function Home() {
-  const [token, setToken] = useState<string>("");
-
-  const fetchToken = async () => {
-    const res = await fetch("/api/auth");
-    const json = await res.json();
-    console.log(json);
-    setToken(json.token);
-  };
-
-  return (
-    <div>
-      <h1>My Homepage</h1>
-      <p>
-        <a href="/auth/login">Login</a>
-      </p>
-      <p>
-        <a href="/auth/logout">Logout</a>
-      </p>
-      <Button className="my-3" onClick={() => fetchToken()}>
-        Get token
-      </Button>
-      <Textarea value={token} rows={10} onChange={() => {}} />
-      <p className="text-lg text-red-500">This should be red</p>
-    </div>
-  );
+  return <></>;
 }
