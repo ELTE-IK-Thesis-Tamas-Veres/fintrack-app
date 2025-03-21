e492460ec067512986b804f3c3aa68aea936ad4b
veresur <veres.tamas@pm.me>
2025-03-21 19:26:31 +0100

    removed unused code

--- src/components/record/RecordsDataTableColumns.tsx
+++ src/components/record/RecordsDataTableColumns.tsx
@@ -23,28 +23,6 @@ export const GetRecordsDataTableColumns = (
   setRecordToEdit: (record: Record) => void
 ) => {
   const RecordsDataTableColumns: ColumnDef<Record>[] = [
-    // {
-    //   id: "select",
-    //   header: ({ table }) => (
-    //     <Checkbox
-    //       checked={
-    //         table.getIsAllPageRowsSelected() ||
-    //         (table.getIsSomePageRowsSelected() && "indeterminate")
-    //       }
-    //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-    //       aria-label="Select all"
-    //     />
-    //   ),
-    //   cell: ({ row }) => (
-    //     <Checkbox
-    //       checked={row.getIsSelected()}
-    //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-    //       aria-label="Select row"
-    //     />
-    //   ),
-    //   enableSorting: false,
-    //   enableHiding: false,
-    // },
     {
       accessorKey: "category",
       header: "Category",
